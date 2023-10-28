ac47c1fa510736d8d39b87300b6eff78646a2000
jarrodwatts <jarrod@thirdweb.com>
2023-10-28 12:56:53 +1100

    Add native wind

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { ConnectWallet, ThirdwebProvider } from "@thirdweb-dev/react-native";
-import { Button, StyleSheet, Text, useColorScheme, View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { Button, Text, View } from "react-native";
 import { TW_CLIENT_ID } from "@env";
 import { myWallet } from "./secp256r1-wallet/MyConnector";
 import * as LocalAuthentication from "expo-local-authentication";
@@ -19,18 +18,10 @@ const App = () => {
 };
 
 const AppInner = () => {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const textStyles = {
-    color: isDarkMode ? Colors.white : Colors.black,
-    ...styles.heading,
-  };
-
   return (
-    <View style={styles.view}>
-      <Text style={textStyles}>React Native thirdweb starter</Text>
+    <View className="bg-black h-full w-full">
+      <Text>React Native thirdweb starter</Text>
       <ConnectWallet />
-
       {/* Button that calls whether or not LocalAuthentication.hasHardwareAsync() */}
       <Button
         title="Check for biometrics"
@@ -45,20 +36,5 @@ const AppInner = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  view: {
-    height: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    alignContent: "center",
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-});
 
 export default App;
